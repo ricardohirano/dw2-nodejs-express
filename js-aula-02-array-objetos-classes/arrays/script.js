991710ef3bbe97a5d1d1be167dd74d58ac9db41b
DSM2 <fatec-dsm2@academico.intra>
2026-03-04 19:22:13 -0300

    aula array

---
+++ js-aula-02-array-objetos-classes/arrays/script.js
@@ -0,0 +1,7 @@
+// Declarand e exibindo itens em um array
+
+let produtos =[];
+
+document.write(typeof produtos); /// mostrar que no javascript é um objeto
+ let products = ["computador", "notebook", "celular", "tablet"];
+ documents.write(`<p>${products}</p>`);
