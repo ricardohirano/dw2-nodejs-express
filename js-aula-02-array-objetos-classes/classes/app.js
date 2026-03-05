db61ee5d98f57dbb86c615366ed8c4881c484d5a
Ricardo Hirano <ricardohiranorgt@gmail.com>
2026-03-05 19:27:36 -0300

    aula classes

---
+++ js-aula-02-array-objetos-classes/classes/app.js
@@ -0,0 +1,24 @@
+// classe no javascipt
+// Nome da classe sempre tem quie comecar com letras maiuscula
+class Carro{
+    // definindo os atributos
+    constructor(marca, modelo, ano) {
+        // this é ma referencia as instancials que serao criadas atraves dessa classe
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    //definindo os metodos
+    buzinar() {
+        return "Beep!, Beep!";
+    }
+}
+
+// Instanciando objetos
+
+const carroPopular = new Carro("fiat", "uno", "2012");
+
+document.write(`<p>O carro ${carroPopular.marca} modelo ${carroPopular.modelo} é do ano ${carroPopular.ano} e quando buzina faz ${carroPopular.buzinar()}</p>`);
+
+
+// desafio carro esportivo (chevvolet, camaro, 2020) +turbo() return "vrumm"
